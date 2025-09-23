0b339e52d4096fd3f96860a51a5190f32f5ef79a
Aleks Wojewoda <alekswojewoda@live.com>
2025-09-23 16:44:55 -0700

    Update portal names in PortalManager to include "valley of the fire drinker"
    - Added "valley of the fire drinker" to the portal names array, enhancing navigation options within the game environment.
    - Ensured existing portal names remain intact for a consistent user experience.

--- PortalManager.cs
+++ PortalManager.cs
@@ -24,7 +24,8 @@ namespace BetterFollowbotLite
                     "ascendancy chamber", "arena", "the pit", "warden's quarters", "portal", "the ring of blades",
                     "combat", "merveil's lair", "merveils lair", "the weaver's nest", "stairs", "pyramid apex",
                     "tower rooftop", "caldera of the king", "the black core", "the black heart", "sanctum of innocence",
-                    "the chamber of innocence", "tukohama's keep", "the karui fortress", "prison rooftop"
+                    "the chamber of innocence", "tukohama's keep", "the karui fortress", "prison rooftop",
+                    "valley of the fire drinker"
                 }, "Arena"
             }
         };

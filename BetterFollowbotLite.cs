3d306c6ded781d55a10e6d1db24144109fd27d8a
Aleks Wojewoda <alekswojewoda@live.com>
2025-09-23 11:49:30 -0700

    ball knowledge

--- BetterFollowbotLite.cs
+++ BetterFollowbotLite.cs
@@ -13,7 +13,6 @@ using ExileCore.Shared;
 using ExileCore.Shared.Enums;
 using SharpDX;
 using BetterFollowbotLite.Skills;
-using BetterFollowbotLite.Skill;
 using BetterFollowbotLite.Automation;
 using BetterFollowbotLite.Core.TaskManagement;
 using BetterFollowbotLite.Core.Movement;

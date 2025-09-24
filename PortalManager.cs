70adcf9e25455eb8bd602c39b31656eb90ba1eb7
Aleks Wojewoda <alekswojewoda@live.com>
2025-09-23 20:14:23 -0700

    tfe

--- PortalManager.cs
+++ PortalManager.cs
@@ -26,7 +26,7 @@ namespace BetterFollowbotLite
                     "tower rooftop", "caldera of the king", "the black core", "the black heart", "sanctum of innocence",
                     "the chamber of innocence", "tukohama's keep", "the karui fortress", "prison rooftop",
                     "valley of the fire drinker", "the cloven pass", "valley of the soul drinker", "the spawning ground",
-                    "passage", "the brine king's throne", 
+                    "passage", "the brine king's throne", "the fortress encampment"
                 }, "Arena"
             }
         };
